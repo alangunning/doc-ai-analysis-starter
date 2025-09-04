218a6c5a09222bbf30256869ab2cdf8461c3bf4b
Alan Gunning <alangunning@gmail.com>
2025-09-04 02:38:36 +0100

    Use main CLI entry point for script and module

--- doc_ai/__main__.py
+++ doc_ai/__main__.py
@@ -1,8 +1,8 @@
-"""Run the doc_ai CLI with `python -m doc_ai`."""
+"""Run the doc_ai CLI with ``python -m doc_ai``."""
 
-from doc_ai.cli import app
+from doc_ai.cli import main
 
 
 if __name__ == "__main__":
-    app()
+    main()
 
